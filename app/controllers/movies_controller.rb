a2adcaa6b189389e490bf7f5f29b2577e26e38c8
Prabesh <iprabesh@hotmail.com>
2018-02-21 08:50:06 -0600

    Adding form for Edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,5 +7,19 @@ class MoviesController < ApplicationController
 	def show
 		@movie = Movie.find(params[:id])
 	end
+
+	def edit
+		@movie = Movie.find(params[:id])
+	end
 	
+	def update
+		@movie = Movie.find(params[:id])
+		movie_params = params[:movie].permit(:title, :description, :released_on,
+						:rating, :total_gross)
+		@movie.update(movie_params)
+		@movie.save
+		redirect_to @movie
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 	root "movies#index"
 
-	get "movies" => "movies#index"
-	get "movies/:id" => "movies#show", as: "movie"
+	resources :movies
+	
 
 end
